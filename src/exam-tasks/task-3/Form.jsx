63451e9a355d18a19aed60af41f1ff141a411123
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-19 12:15:25 +0200

    Handle submit and display feedback message according to age

--- src/exam-tasks/task-3/Form.jsx
+++ src/exam-tasks/task-3/Form.jsx
@@ -5,12 +5,26 @@ function Form() {
     const [lastName, setLastName] = useState('');
     const [age, setAge] = useState(null);
 
+    const [formData, setFormData] = useState();
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormData({ 
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+        })
+        setMessage(true);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Vardas" onChange={(e) => setFirstName(e.target.value)} />
             <input type="text" placeholder="Pavardė" onChange={(e) => setLastName(e.target.value)}/>
             <input type="number" placeholder="Amžius" onChange={(e) => setAge(e.target.value)}/>
             <button>Pateikti</button>
+            {message && <div>{formData.age >= 18 ? "Dėkojame, kad užsiregistravote" : "Deja, registracija negalima. Registruotis galima tik nuo 18 metų"}</div>}
         </form>
     );
 }
