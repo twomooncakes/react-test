bb6dc0bcd7a39b8e58386d86e0927f70393d0126
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-22 08:48:05 +0200

    Refactor task 2 style to use css module

--- src/exam-tasks/task-2/Categories.jsx
+++ src/exam-tasks/task-2/Categories.jsx
@@ -1,11 +1,13 @@
+import css from './Categories.module.css';
+
 function Categories({productCategories}) {
     return (
-        <main id="task2">
+        <main className={css.container}>
             <h1>Categories</h1>
-            <div className="categories-container">
+            <div className={css['categories-container']}>
                 {productCategories.map((category) => {
                     return (
-                        <div className="category-wrapper">
+                        <div className={css['category-wrapper']}>
                             <img src={category.image} />
                             <h2>{category.title}</h2>
                         </div>
