7d36c1e359837f457e84dcba57d23d8242282beb
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-22 15:15:38 +0200

    Display todos from API + css module styling

--- src/exam-tasks/task-9/Card.js
+++ src/exam-tasks/task-9/Card.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import css from './Card.module.css';
 
 export default class Card extends Component {
-  render() {
-    return <div>Card Component</div>;
-  }
+    render() {
+        return (
+            <div className={css['todo-card']}>
+                <p>{this.props.todo.title}</p>
+                <span className={this.props.todo.completed ? css.completed : css.unfinished}>completed: {this.props.todo.completed ? 'yes' : 'no'}</span>
+            </div>
+        )
+        
+    }
 }
--- src/exam-tasks/task-9/Task9.js
+++ src/exam-tasks/task-9/Task9.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 export default class Task9 extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Task 9</h3>
-        {/*<Card />*/}
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [],
+        };
+    }
+
+    getTodos = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await resp.json();
+        console.log(data.slice(0,5))
+        this.setState({ todos: data.slice(0,5) })
+    }
+
+    componentDidMount() {
+        this.getTodos();
+    }
+
+    render() {
+        return (
+        <div>
+            <h3>Task 9</h3>
+            {this.state.todos.map((todo) => <Card key={todo.id} todo={todo}></Card>)}
+        </div>
+        );
+    }
 }
 
 /* Task 9
