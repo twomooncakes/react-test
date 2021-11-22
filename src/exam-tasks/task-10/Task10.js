aec624a1584e2f127a79d949d39a7dd9ffe1c435
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-22 16:49:05 +0200

    Explain Task 10

--- src/exam-tasks/task-10/Task10.js
+++ src/exam-tasks/task-10/Task10.js
@@ -8,4 +8,7 @@ Because it didn’t get the context.
 
 Jūsų paaiškinimas:
 
+React'o kontekstas leidžia dalintis informacija su bet kuriuo komponentu. Paprastai, tai būtų daroma su perdavimu iš tėvinio elemento į vaikinį(prop drilling), bet konteksto naudojimas leidžia to išvengti ir viską supaprastina. Norint panaudoti kontekstą, jį aprašome ir suteikiame prieigą prie konteksto reikšmių bet kuriam komponentui pagal poreikį. 
+React'o komponentas nesuprato anekdoto todėl, kad greičiausiai negali pasiekti konteksto failo ir jo reikšmių, nes Context Provider komponentas jo neapima ir/arba komponento faile trūksta konteksto importo ir atitinkamo kintamojo.
+
 */
