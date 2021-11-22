699fc0480500a6e2d2005787762a6121264f8821
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-22 11:03:06 +0200

    Implement theme change for task 7 and add styles

--- src/exam-tasks/task-7/Task7.js
+++ src/exam-tasks/task-7/Task7.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import css from './Task7.module.css';
 
 function Task7() {
-  return (
-    <div>
-      <h3>Task 7</h3>
-    </div>
-  );
+    const task7Ref = useRef(null);
+
+    const lightTheme = () => {
+        task7Ref.current.style.backgroundColor = '#f1f1f1';
+        task7Ref.current.style.color = '#000';
+    };
+    const darkTheme = () => {
+        task7Ref.current.style.backgroundColor = '#000';
+        task7Ref.current.style.color = '#f1f1f1';
+    };
+
+    return (
+        <div ref={task7Ref}>
+            <h3>Task 7</h3>
+            <div className={css.container}>
+                <h1>Lorem ipsum catticus</h1>
+                <p>Birman. Scottish fold cheetah leopard yet persian but siberian, and cornish rex but kitty. Siberian kitty abyssinian or american bobtail birman. Cheetah kitty for bobcat and tomcat. Munchkin tiger and tom but leopard yet jaguar, bengal. Kitty cheetah, tom. Kitten jaguar, russian blue yet singapura jaguar. Siberian kitty abyssinian or american bobtail birman.</p>
+                <div className={css['task7-btns']}>
+                    <button  onClick={lightTheme}>Light theme</button>
+                    <button onClick={darkTheme}>Dark theme</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Task7;
