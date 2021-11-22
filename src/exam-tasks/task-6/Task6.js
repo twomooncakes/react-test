016e46568acf594862a4a956bd90ca6b6ab0ad05
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-22 09:56:55 +0200

    Fetch and display table data, add css module styles for task 6

--- src/exam-tasks/task-6/Task6.js
+++ src/exam-tasks/task-6/Task6.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import css from './Task6.module.css';
 
 function Task6() {
-  return (
-    <div>
-      <h3>Task 6</h3>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await resp.json();
+        setUsers(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    return (
+        <div className={css.container}>
+            <h3>Task 6</h3>
+            <table>
+                <thead>
+                    <tr className={css.header}>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>address</th>
+                        <th>phone</th>
+                        <th>company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.length > 0 && users.map((user) => {
+                        return (
+                            <tr>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{`${user.address.street}, ${user.address.city}`}</td>
+                                <td>{user.phone}</td>
+                                <td>{user.company.name}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Task6;
